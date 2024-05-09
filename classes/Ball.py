ed3a715a104e251c23094f24b123d0c53d293c88
Sergey Zholudev <82098764+SergeyZholudev@users.noreply.github.com>
2024-05-09 12:16:31 +0300

    Изменен класс Ball

--- classes/Ball.py
+++ classes/Ball.py
@@ -2,11 +2,18 @@ import pygame
 from pygame.locals import *
 import random
 
-
 class Ball:
     def __init__(self, window, windowWidth, windowHeight):
         self.window = window
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
         self.image = pygame.image.load("images/ball.png")
+
+        
+        # прямоугольник состоит из x и y
+        ballRect = self.image.get_rect()
+        self.ballWindth = ballRect.width()
+        self.ballHeight = ballRect.height()
+        self.MaxWidth = self.windowWidth - self.ballWindth
+        self.MaxWidth = self.windowHeight - self.ballHeight
         
