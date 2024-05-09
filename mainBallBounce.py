1e19889bc45f3acc87750eb373770d5f4423f048
Sergey Zholudev <serg.zholudev@gmail.com>
2024-05-09 21:44:01 +0300

    С помощью цикла for in создаём 3 мяча, формируем из них список, в результате на экране 3 мяча.

--- mainBallBounce.py
+++ mainBallBounce.py
@@ -11,14 +11,18 @@ COLOR = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
+N_BALLS = 3
 
 # инициализируем переменные окна
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-# инициализируем переменные
-oBall = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+# создаём список состоящий из оъектов
+ballList = []
+for i in range(N_BALLS):
+    oBall = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+    ballList.append(oBall)
 
 # бесконечные цикл
 while True:
@@ -28,13 +32,17 @@ while True:
             sys.exit()
 
     # выполняем действия в рамках фрейма
-    oBall.update()
+
+    #для кадждого мяча определяем координаты и движение
+    for item in ballList:
+        item.update()
 
     # очищаем экран
     window.fill(COLOR)
 
-    # рисуем мяч
-    oBall.draw()
+    # рисуем мячи из списка 
+    for item in ballList:
+        item.draw()
 
     # обновляем окно
     pygame.display.update()
