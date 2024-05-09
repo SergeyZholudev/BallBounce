f9848025ad73af78f0de4c0b6fe3f4350aa0d12b
Sergey Zholudev <serg.zholudev@gmail.com>
2024-05-09 15:33:08 +0300

    Создан class Ball, описаны атрибуты, добавлены методы.

--- classes/Ball.py
+++ classes/Ball.py
@@ -7,7 +7,7 @@ class Ball:
         self.window = window
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
-        self.image = pygame.image.load("images/ball.png")
+        self.image = pygame.image.load("/images/ball.png")
         
         # прямоугольник состоит из x и y
         ballRect = self.image.get_rect()
@@ -16,3 +16,27 @@ class Ball:
         self.MaxWidth = self.windowWidth - self.ballWindth
         self.MaxWidth = self.windowHeight - self.ballHeight
 
+        # выбираем произвольную начальную позицию
+        self.x = random.randint(0, self.MaxWidth)
+        self.y = random.randint(0, self.ballHeight)
+
+        # определяем скорость движения объекта
+        speedValues = [-4, -3, -2, -1, 1, 2, 3, 4]
+        self.speedX = random.choice(speedValues)
+        self.speedY = random.choice(speedValues)
+
+    def update(self):
+        # проверяем столкновение объекта со стенками, если true
+        # меняем направление
+        if self.x < 0 or (self.x >= self.MaxWidth):
+            self.speedX = -self.speedX
+
+        if self.x < 0 or (self.y >= self.MaxHeight):
+            self.speedY = -self.speedY
+
+        # обновляем координату x и y
+        self.x = self.x + self.speedX
+        self.y = self.y + self.speedY
+
+    def draw(self):
+        self.window.blit(self.image, (self.x, self.y))
